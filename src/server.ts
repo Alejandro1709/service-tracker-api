296335e599ff0556ea7e53028c0ae32e106e1d76
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-05-10 20:54:19 -0500

    Add: error handling basic

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import { globalErrorHandler } from './middlewares/error';
 
 const app = express();
 
@@ -8,5 +9,11 @@ app.use(morgan('dev'));
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Ok' });
 });
+
+app.use((req, res, next) => {
+  res.status(404).json({ status: 'fail', message: 'This route does not exists' });
+});
+
+app.use(globalErrorHandler);
 
 export default app;
