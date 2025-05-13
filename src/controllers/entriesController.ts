41834e2b234abb1e4df6eb60a723394c61f8b67a
Alejandro LR <alejandro.lr.developer@hotmail.com>
2025-05-13 12:29:32 -0500

    Add: Entry CRUD

--- src/controllers/entriesController.ts
+++ src/controllers/entriesController.ts
@@ -2,6 +2,7 @@ import type { NextFunction, Request, Response } from 'express';
 import catchAsync from '../utils/catchAsync';
 import createEntrySchema from '../schemas/entry.schema';
 import Entry from '../models/Entry';
+import AppError from '../utils/AppError';
 
 export const getEntries = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   const entries = await Entry.find();
@@ -10,7 +11,13 @@ export const getEntries = catchAsync(async (req: Request, res: Response, next: N
 });
 
 export const getEntry = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).json({ message: 'Ok' });
+  const entry = await Entry.findById(req.params.id);
+
+  if (!entry) {
+    return next(new AppError('Entry not found', 404));
+  }
+
+  res.status(200).json({ status: 'success', entry });
 });
 
 export const createEntry = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
@@ -18,5 +25,31 @@ export const createEntry = catchAsync(async (req: Request, res: Response, next: 
 
   const entry = await Entry.create(request);
 
+  res.status(201).json({ status: 'success', entry });
+});
+
+export const updateEntry = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  // TODO: Change schema to updateSchema
+  const request = createEntrySchema.parse(req.body);
+
+  const entry = await Entry.findByIdAndUpdate(req.params.id, request, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!entry) {
+    return next(new AppError('Entry not found', 404));
+  }
+
   res.status(200).json({ status: 'success', entry });
+});
+
+export const deleteEntry = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const entry = await Entry.findByIdAndDelete(req.params.id);
+
+  if (!entry) {
+    return next(new AppError('Entry not found', 404));
+  }
+
+  res.status(200).json({ status: 'success', entry: null });
 });
